29b47e885ad5e194d24d6035d95e1697ef68fdc4
Petteri Jekunen <petteri.jekunen@tuni.fi>
2025-08-29 00:09:36 +0300

    Code revolution ...

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Let's have fun with Java ...");
     }
 }
